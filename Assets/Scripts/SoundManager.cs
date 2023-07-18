b4aeabe36ff766016edaf04762558ec43b27d261
Alan Jantz <alan_f_jantz@hotmail.com>
2023-07-18 20:11:04 -0300

    Ajuste audio e fontes

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,16 +1,18 @@
+using System;
 using System.Collections;
+using System.Linq;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
     private void Start()
     {
-        StartCoroutine(playEnvironmentMusic());
+        StartCoroutine(PlayEnvironmentMusic());
     }
 
-    IEnumerator playEnvironmentMusic()
+    private IEnumerator PlayEnvironmentMusic()
     {
-        foreach (var audio in MainAssets.Instance.environmentMusic)
+        foreach (var audio in MainAssets.Instance.environmentMusic.OrderBy(_ => Guid.NewGuid()))
         {
             GetComponent<AudioSource>().clip = audio;
             GetComponent<AudioSource>().Play();
